47b7a5e21ad1ddda4a3e8b8d17af35d093463569
Jonathan Norberg <Jonathannorberg95@gmail.com>
2024-03-13 14:00:57 +0100

    Added component for header

---
+++ Header.jsx
@@ -0,0 +1,14 @@
+
+export const Header = () => {
+    return < >
+        <header class="header">
+            <div class="wrapper">
+                <h1>TransMetroPolitan</h1>
+                <ul class="authors">
+                    <li>Darick Robertson</li>
+                    <li>Warren Ellis</li>
+                </ul>
+            </div>
+        </header>
+    </>
+}
